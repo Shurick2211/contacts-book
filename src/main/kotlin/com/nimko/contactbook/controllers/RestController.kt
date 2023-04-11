ff5db0cd6a45649965889ab799a58ec4a445acf2
sn <shurick2211@gmail.com>
2023-04-11 21:20:47 +0300

    1 Create + swagger /

--- src/main/kotlin/com/nimko/contactbook/controllers/RestController.kt
+++ src/main/kotlin/com/nimko/contactbook/controllers/RestController.kt
@@ -2,6 +2,10 @@ package com.nimko.contactbook.controllers
 
 import com.nimko.contactbook.dto.PersonDto
 import com.nimko.contactbook.services.PersonsServices
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -9,6 +13,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/contacts")
+@OpenAPIDefinition(info =
+Info(
+    title = "Contacts Book",
+    version = "0.0.1",
+    description = "My Test API for UpUp",
+    contact = Contact(name = "Shurick", email = "shurick2211@gmail.com")
+)
+)
+@Tag(name = "Controller",description = "My ToDo Controller")
 class RestController @Autowired constructor(
     val service:PersonsServices
 ){
